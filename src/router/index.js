0d94bf31d3a442a798ef8b0bd3fdd6caebc4c0a3
alihanayd <aaydin184@posta.pau.edu.tr>
2023-08-26 17:46:27 +0300

    update(router):: results & high scores

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import QuestionsView from "@/views/QuestionsView.vue";
+import ResultsView from "@/views/ResultsView.vue";
+import HighScoresView from "@/views/HighScoresView.vue";
 
 Vue.use(VueRouter)
 
@@ -16,6 +18,16 @@ const routes = [
         name: 'question',
         component: QuestionsView
     },
+    {
+        path: '/results',
+        name: 'results',
+        component: ResultsView
+    },
+    {
+        path: '/highscores',
+        name: 'highscores',
+        component: HighScoresView
+    }
 ]
 
 const router = new VueRouter({
