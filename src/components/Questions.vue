db4cb152c3cee9cfd907371db2391c1c89320db2
alihanayd <aaydin184@posta.pau.edu.tr>
2023-08-26 17:45:25 +0300

    update():: quiz component

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div v-for="(question, index) in questions" :key="index">
+  <div class="flex justify-center items-center h-screen bg-violet-700">
+    <!-- <div v-for="(question, index) in questions" :key="index">
       <div>
         <p>{{ question.question }}</p>
       </div>
@@ -10,19 +10,95 @@
         <label :for="index + '_' + question.id">{{ answer.answer }}</label>
       </div>
 
+    </div> -->
+    <div class="flex flex-col items-center bg-violet-900 p-10 rounded-lg">
+      <div class="text-center">
+        <p class="text-white">{{ questions[currentIndex]?.question }}</p>
+      </div>
+      <div class="grid grid-cols-2 gap-4 mt-4">
+        <div
+          v-for="(answer, index) in questions[currentIndex]?.options"
+          :key="index"
+          class="flex items-center px-4 py-2 border text-white border-gray-200 rounded user-select-none cursor-pointer hover:bg-blue-700 dark:hover:bg-gray-600 dark:border-gray-600"
+          :class="{ 'bg-blue-700': index === activeIndex }"
+          @click="checkAnswer(answer.correct, index)"
+        >
+          {{ answer.answer }}
+          <!-- <input
+            type="radio"
+            :key="index + '_' + answer.answer"
+            :id="index + '_' + answer.answer"
+            :name="questions[currentIndex]?.id"
+            :value="answer.correct"
+            :checked="false"
+            v-model="isAnswerCorrect"
+            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+          />
+          <label
+            :for="index + '_' + answer.answer"
+            class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >{{ answer.answer }}</label
+          > -->
+        </div>
+      </div>
+      <div class="mt-4">
+        <button
+          @click="nextQuestion"
+          class="w-52 h-8 bg-red-600 text-white rounded-md cursor-pointer"
+          type="button"
+        >
+          Continue
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       questions: [],
+      currentIndex: 0,
+      score: 0,
+      isAnswerCorrect: null,
+      activeIndex: null,
     };
   },
   mounted() {
-    this.questions = this.$store.getters['questions/getQuestions'];
+    this.questions = this.$store.getters["questions/getQuestions"];
+  },
+  methods: {
+    nextQuestion() {
+      if (this.isAnswerCorrect) {
+        this.score++;
+      }
+      this.activeIndex = null;
+      this.isAnswerCorrect = null;
+      this.currentIndex < this.questions.length - 1
+        ? this.currentIndex++
+        : this.setScore();
+    },
+    setScore() {
+      axios
+        .post(
+          "https://quiz-app-f3384-default-rtdb.firebaseio.com/scores.json",
+          {
+            name: this.$store.getters["user/getName"],
+            score: this.score,
+          }
+        )
+        .then((response) => {
+          console.log(response);
+          this.$router.push("/results");
+        })
+        .catch((error) => console.log(error));
+    },
+    checkAnswer(answer, index) {
+      this.isAnswerCorrect = answer;
+      this.activeIndex = index;
+    },
   },
 };
 </script>
--- src/components/QuizForm.vue
+++ src/components/QuizForm.vue
@@ -1,43 +1,60 @@
 <template>
   <div class="flex flex-col justify-around items-center h-screen bg-violet-700">
-    <app-title/>
+    <app-title />
     <div class="flex justify-center flex-col">
-      <input
+      <div class="mb-5 flex flex-col">
+        <input
           v-model="name"
-          class="mb-5 rounded p-1"
+          class="rounded p-1"
           placeholder="Please enter your name"
           type="text"
-      />
-
-      <select
+        />
+        <span class="text-red-500" v-if="isValidName">Please enter a name</span>
+      </div>
+      <div class="mb-5">
+        <select
           v-model="selectedCategory"
-          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-5"
-      >
-        <option disabled value="">Please Choose a Category</option>
-        <option
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+          <option disabled value="">Please Choose a Category</option>
+          <option
             v-for="category in categories"
             :value="category"
             :key="category.number"
+          >
+            {{ category.name }}
+          </option>
+        </select>
+        <span class="text-red-500" v-if="isValidCategory"
+          >Please Choose a Category</span
         >
-          {{ category.name }}
-        </option>
-      </select>
-
-      <select
+      </div>
+      <div class="mb-5">
+        <select
           v-model="difficulty"
-          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-5"
-      >
-        <option disabled value="">Please Choose a Difficulty</option>
-        <option value="easy">Easy</option>
-        <option value="medium">Medium</option>
-        <option value="hard">Hard</option>
-      </select>
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+          <option disabled value="">Please Choose a Difficulty</option>
+          <option value="easy">Easy</option>
+          <option value="medium">Medium</option>
+          <option value="hard">Hard</option>
+        </select>
+        <span class="text-red-500" v-if="isValidDifficulty"
+          >Please Choose a Difficulty</span
+        >
+      </div>
 
       <button
-          @click="getQuestions"
-          class="w-52 h-8 bg-red-600 text-white rounded-md cursor-pointer"
+        @click="getQuestions"
+        class="w-52 h-8 bg-red-600 text-white rounded-md cursor-pointer mb-5"
       >
         Continue
+      </button>
+      <button
+        @click="$router.push('/highscores')"
+        class="w-52 h-8 bg-emerald-600 text-white rounded-md cursor-pointer"
+      >
+        High Scores
       </button>
     </div>
   </div>
@@ -56,88 +73,119 @@ export default {
       questions: [],
       difficulty: "",
       categories: [
-        {name: "general knowledge", number: 9},
-        {name: "books", number: 10},
-        {name: "film", number: 11},
-        {name: "music", number: 12},
-        {name: "television", number: 14},
-        {name: "video games", number: 15},
-        {name: "computers", number: 18},
-        {name: "mathematics", number: 19},
-        {name: "sports", number: 21},
-        {name: "history", number: 23},
-        {name: "art", number: 25},
-        {name: "celebrities", number: 26},
-        {name: "animals", number: 27},
-        {name: "vehicles", number: 28},
-        {name: "comics", number: 29},
-        {name: "gadgets", number: 30},
-        {name: "japanese anime & manga", number: 31},
-        {name: "cartoon & animations", number: 32},
+        { name: "general knowledge", number: 9 },
+        { name: "books", number: 10 },
+        { name: "film", number: 11 },
+        { name: "music", number: 12 },
+        { name: "television", number: 14 },
+        { name: "video games", number: 15 },
+        { name: "computers", number: 18 },
+        { name: "mathematics", number: 19 },
+        { name: "sports", number: 21 },
+        { name: "history", number: 23 },
+        { name: "art", number: 25 },
+        { name: "celebrities", number: 26 },
+        { name: "animals", number: 27 },
+        { name: "vehicles", number: 28 },
+        { name: "comics", number: 29 },
+        { name: "gadgets", number: 30 },
+        { name: "japanese anime & manga", number: 31 },
+        { name: "cartoon & animations", number: 32 },
       ],
       selectedCategory: "",
       name: "",
       questionSplit: {},
+      isValidName: false,
+      isValidDifficulty: false,
+      isValidCategory: false,
     };
   },
   methods: {
-
+    checkError() {
+      this.resetErrors();
+      if (this.name === "") {
+        this.isValidName = true;
+        return true;
+      }
+      if (this.selectedCategory === "") {
+        this.isValidCategory = true;
+        return true;
+      }
+      if (this.difficulty === "") {
+        this.isValidDifficulty = true;
+        return true;
+      }
+      return false;
+    },
+    resetErrors() {
+      this.isValidName = false;
+      this.isValidDifficulty = false;
+      this.isValidCategory = false;
+    },
     // "https://opentdb.com/api.php?amount=10&category=" + this.selectedCategory.number + "&difficulty=" + this.difficulty + "&type=multiple"
     getQuestions() {
+      if (this.checkError()) {
+        return;
+      }
       axios
-          .get("https://opentdb.com/api.php", {
-            params: {
-              amount: 10,
-              category: this.selectedCategory.number,
-              difficulty: this.difficulty,
-              type: "multiple",
-            },
-          })
-          .then(async (response) => {
-            this.questions = response.data.results;
-            console.log(this.questions);
-            const questionsStoreArray = [];
-            this.questions.map((questionObject, index) => {
-              const options = [
-                {
-                  answer: questionObject.correct_answer,
-                  correct: true,
-                },
-                {
-                  answer: questionObject.incorrect_answers[0],
-                  correct: false,
-                },
-                {
-                  answer: questionObject.incorrect_answers[1],
-                  correct: false,
-                },
-                {
-                  answer: questionObject.incorrect_answers[2],
-                  correct: false,
-                },
-              ]
-              const question = {
-                id: index,
-                question: questionObject.question,
-                options: this.randomizeAnswers(options)
-              };
-              questionsStoreArray.push(question);
-            });
-            await this.$store.dispatch('questions/setQuestions', questionsStoreArray);
-            const storeQuestions = this.$store.getters['questions/getQuestions'];
+        .get("https://opentdb.com/api.php", {
+          params: {
+            amount: 10,
+            category: this.selectedCategory.number,
+            difficulty: this.difficulty,
+            type: "multiple",
+          },
+        })
+        .then(async (response) => {
+          this.questions = response.data.results;
+          console.log(this.questions);
+          const questionsStoreArray = [];
+          this.questions.map((questionObject, index) => {
+            const options = [
+              {
+                answer: questionObject.correct_answer,
+                correct: true,
+              },
+              {
+                answer: questionObject.incorrect_answers[0],
+                correct: false,
+              },
+              {
+                answer: questionObject.incorrect_answers[1],
+                correct: false,
+              },
+              {
+                answer: questionObject.incorrect_answers[2],
+                correct: false,
+              },
+            ];
+            const question = {
+              id: index,
+              question: questionObject.question,
+              options: this.randomizeAnswers(options),
+            };
+            questionsStoreArray.push(question);
+          });
+          console.log(questionsStoreArray);
+          await this.$store.dispatch(
+            "questions/setQuestions",
+            questionsStoreArray
+          );
+          const storeQuestions = this.$store.getters["questions/getQuestions"];
 
-            storeQuestions.length > 0 && await this.$router.push('/questions');
-            // storeQuestions.length > 0 ? await this.$router.push('/questions') : alert('Please select a category and difficulty');
+          storeQuestions.length > 0 && (await this.$router.push("/questions"));
+          // storeQuestions.length > 0 ? await this.$router.push('/questions') : alert('Please select a category and difficulty');
 
-            // if (storeQuestions.length > 0) {
-            //   await this.$router.push('/questions');
-            // }
-
-
-          })
-          .catch((error) => {
-            alert(error.message)
-          })
+          // if (storeQuestions.length > 0) {
+          //   await this.$router.push('/questions');
+          // }
+          this.$store.dispatch("user/setName", this.name);
+          console.log(this.name);
+          console.log(this.$store.getters["user/getName"]);
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
     },
     randomizeAnswers(options) {
       return options.sort(() => Math.random() - 0.5);
